e6ce8dbccecd6e8cd33c1f395e6c7b14f2467963
michaelsupercode <96441437+michaelsupercode@users.noreply.github.com>
2024-03-22 12:42:46 +0100

    Update jest.config.js

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,11 +1,1 @@
-// jest.config.js
-export default {
-  setupFiles: ["<rootDir>/setupTests.js"],
-  transform: {},
-  testEnvironment: "node",
-  globals: {
-    "ts-jest": {
-      useESM: true,
-    },
-  },
-};
+
